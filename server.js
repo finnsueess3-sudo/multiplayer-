fdad066980c1ca73ae3716bc328e0784833f63a4
finnsueess3-sudo <finn.sueess3@gmail.com>
2025-11-10 11:15:44 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,24 +6,45 @@ const io = require('socket.io')(http);
 app.use(express.static('public'));
 
 const players = {};
+const buildings = []; // Gebäude mit Widerstand
+
+// Beispielgebäude
+for(let i=-50;i<=50;i+=20){
+    for(let j=-50;j<=50;j+=20){
+        if(Math.random() > 0.5){
+            buildings.push({x:i, z:j, width:10, depth:10, height:Math.random()*15+5, hp:100});
+        }
+    }
+}
 
 io.on('connection', (socket) => {
-    console.log('Ein Spieler verbunden:', socket.id);
+    console.log('Spieler verbunden:', socket.id);
 
-    // Spieler initialisieren
-    players[socket.id] = { x: 0, y: 0, z: 0, rotation: 0 };
+    players[socket.id] = {x:0,y:1,z:0,rotation:0};
 
-    // Sende allen Spielern die aktuellen Spieler-Positionen
     socket.emit('currentPlayers', players);
-    socket.broadcast.emit('newPlayer', { id: socket.id, data: players[socket.id] });
+    socket.emit('currentBuildings', buildings);
+    socket.broadcast.emit('newPlayer', {id: socket.id, data: players[socket.id]});
 
     socket.on('playerMovement', (data) => {
         players[socket.id] = data;
-        socket.broadcast.emit('playerMoved', { id: socket.id, data });
+        socket.broadcast.emit('playerMoved', {id: socket.id, data});
+    });
+
+    socket.on('shoot', ({position, direction}) => {
+        // Prüfen ob Gebäude getroffen wird
+        buildings.forEach(b => {
+            let dx = position.x - b.x;
+            let dz = position.z - b.z;
+            if(Math.abs(dx)<b.width/2 && Math.abs(dz)<b.depth/2){
+                b.hp -= 25; // Schaden
+                if(b.hp<=0) console.log("Gebäude zerstört!");
+            }
+        });
+        socket.broadcast.emit('bulletFired', {position, direction});
     });
 
     socket.on('disconnect', () => {
-        console.log('Spieler getrennt:', socket.id);
         delete players[socket.id];
         socket.broadcast.emit('playerDisconnected', socket.id);
     });
