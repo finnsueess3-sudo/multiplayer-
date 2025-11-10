2ac1bdd3cef019b9b6e8cda97bb14e4a091813cf
finnsueess3-sudo <finn.sueess3@gmail.com>
2025-11-10 11:04:17 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const http = require("http").createServer(app);
-const io = require("socket.io")(http, { cors: { origin: "*" } });
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-io.on("connection", socket => {
-  socket.on("move", data => socket.broadcast.emit("move", { id: socket.id, ...data }));
-  socket.on("disconnect", () => io.emit("remove", socket.id));
+const players = {};
+
+io.on('connection', (socket) => {
+    console.log('Ein Spieler verbunden:', socket.id);
+
+    // Spieler initialisieren
+    players[socket.id] = { x: 0, y: 0, z: 0, rotation: 0 };
+
+    // Sende allen Spielern die aktuellen Spieler-Positionen
+    socket.emit('currentPlayers', players);
+    socket.broadcast.emit('newPlayer', { id: socket.id, data: players[socket.id] });
+
+    socket.on('playerMovement', (data) => {
+        players[socket.id] = data;
+        socket.broadcast.emit('playerMoved', { id: socket.id, data });
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Spieler getrennt:', socket.id);
+        delete players[socket.id];
+        socket.broadcast.emit('playerDisconnected', socket.id);
+    });
 });
 
-http.listen(process.env.PORT || 3000);
+http.listen(3000, () => console.log('Server läuft auf Port 3000'));
